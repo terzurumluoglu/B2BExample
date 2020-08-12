ca66911b8489a9e70643bf29903d99bab7e41429
toprakerzurumluoglu <toprakerzurumluoglu@hotmail.com>
2020-08-12 23:34:10 +0300

    Product Servis ve buna bağlı olarak Facade Servis Düzenlendi.

--- src/app/services/facade/facade.service.ts
+++ src/app/services/facade/facade.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
+import { ProductService } from "../product/product.service";
+import { Product } from 'src/app/models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FacadeService {
 
-  constructor() { }
+  private _productService: ProductService;
+  public get productService(): ProductService {
+    if (!this._productService) {
+      this._productService = this.injector.get(ProductService);
+    }
+    return this._productService;
+  }
+
+  constructor(private injector: Injector) { }
+
+  getAllProducts(): Product[] {
+    return this.productService.getAllProducts();
+  }
+
+  getProduct(id: number): Product {
+    return this.productService.getProduct(id);
+  }
+
 }
--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Product, ProductsList } from "../../models/product";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
+  productList = new ProductsList;
   constructor() { }
+
+  getAllProducts(): Product[] {
+    return this.productList.getAllProducts();
+  }
+
+  getProduct(id: number) {
+    return this.getProduct(id);
+  }
 }
